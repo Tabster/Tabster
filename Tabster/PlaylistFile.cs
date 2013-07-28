c7817d62ab50ab56a0798437dd1730ed40da9e25
Nate Shoffner <nate.shoffner@gmail.com>
2013-07-28 15:50:21 -0400

    FileNotFoundException catching.

--- Tabster/PlaylistFile.cs
+++ Tabster/PlaylistFile.cs
@@ -98,6 +98,11 @@ namespace Tabster
                 success = true;
             }
 
+            catch(FileNotFoundException)
+            {
+                
+            }
+
             catch (IOException)
             {
 
