f72cb1d0860dd91abefd756d29c07c798486ec7d
Nate Shoffner <nate.shoffner@gmail.com>
2013-08-22 00:42:44 -0400

    Clean up resources.

--- Tabster/Properties/Resources.Designer.cs
+++ Tabster/Properties/Resources.Designer.cs
@@ -103,6 +103,16 @@ namespace Tabster.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap page_save {
+            get {
+                object obj = ResourceManager.GetObject("page_save", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap printer {
             get {
                 object obj = ResourceManager.GetObject("printer", resourceCulture);
