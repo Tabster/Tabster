f2eabcb610b6947390f020b97172764158b112fc
Nate Shoffner <nate.shoffner@gmail.com>
2013-08-24 01:38:07 -0400

    New resources.

--- Tabster/Properties/Resources.Designer.cs
+++ Tabster/Properties/Resources.Designer.cs
@@ -63,6 +63,16 @@ namespace Tabster.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap arrow_out {
+            get {
+                object obj = ResourceManager.GetObject("arrow_out", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap cursor {
             get {
                 object obj = ResourceManager.GetObject("cursor", resourceCulture);
