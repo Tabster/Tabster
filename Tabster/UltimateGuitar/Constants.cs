57dee8b5de90714ffe0e4a34521861e3878f1bb8
Nate Shoffner <nate.shoffner@gmail.com>
2014-06-10 06:21:55 -0400

    Deprecated.

--- Tabster/UltimateGuitar/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tabster.UltimateGuitar
-{
-    public class Constants
-    {
-        public const string UG_HOME_1 = "http://tabs.ultimate-guitar.com/";
-        public const string UG_HOME_2 = "http://www.ultimate-guitar.com/tabs/";
-    }
-}
