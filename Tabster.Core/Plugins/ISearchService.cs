6447e05b1714d3b73354b94f31a2094d12afc2a5
Nate Shoffner <nate.shoffner@gmail.com>
2014-06-26 17:05:24 -0400

    More plugin documentation.

--- Tabster.Core/Plugins/ISearchService.cs
+++ Tabster.Core/Plugins/ISearchService.cs
@@ -1,9 +1,29 @@
 ﻿namespace Tabster.Core.Plugins
 {
+    /// <summary>
+    /// Tab service which enables searching.
+    /// </summary>
     public interface ISearchService
     {
+        /// <summary>
+        /// Service name.
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// Queries service and returns results based on search parameters.
+        /// </summary>
+        /// <param name="artist">The tab artist.</param>
+        /// <param name="title">The tab title.</param>
+        /// <param name="type">The tab type.</param>
+        /// <returns>Array of results.</returns>
         IRemoteTab[] Search(string artist, string title, TabType? type);
+
+        /// <summary>
+        /// Determines whether a specific TabType is supported by the service.
+        /// </summary>
+        /// <param name="type">The type to check.</param>
+        ///<returns>True if the type is supported by the service; otherwise, False.</returns>
         bool SupportsTabType(TabType type);
     }
 }
--- Tabster.Core/Plugins/ITabParser.cs
+++ Tabster.Core/Plugins/ITabParser.cs
@@ -2,10 +2,28 @@
 
 namespace Tabster.Core.Plugins
 {
+    /// <summary>
+    /// Text-based tab parser.
+    /// </summary>
     public interface ITabParser
     {
+        /// <summary>
+        /// Parser name.
+        /// </summary>
         string Name { get; }
+
+        /// <summary>
+        /// Parses tab from text source.
+        /// </summary>
+        /// <param name="sourceText"></param>
+        /// <returns></returns>
         IRemoteTab ParseTabFromSource(string sourceText);
+
+        /// <summary>
+        /// Determines whether a specified URL matches a specific pattern used by the parser. Used for web-based parsing.
+        /// </summary>
+        /// <param name="url">The URL to check.</param>
+        ///<returns>True if the URL matches the supported pattern; otherwise, False.</returns>
         bool MatchesUrlPattern(Uri url);
     }
 }
