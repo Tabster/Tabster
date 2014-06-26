99e4b7ad3fd827a620b6b910f5490cb1d6f790a2
Nate Shoffner <nate.shoffner@gmail.com>
2014-06-26 03:29:43 -0400

    Plugin-centric code moved from core.

--- Plugins/UltimateGuitar/UltimateGuitarParser.cs
+++ Plugins/UltimateGuitar/UltimateGuitarParser.cs
@@ -1,6 +1,7 @@
 ﻿#region
 
 using System;
+using System.Text.RegularExpressions;
 using HtmlAgilityPack;
 using Tabster.Core;
 using Tabster.Core.Plugins;
@@ -55,8 +56,8 @@ namespace UltimateGuitar
 
             if (contentsNode != null)
             {
-                var contents = Common.StripHTML(contentsNode.InnerHtml);
-                contents = Common.ConvertNewlines(contents);
+                var contents = StripHTML(contentsNode.InnerHtml);
+                contents = ConvertNewlines(contents);
                 return new UltimateGuitarTab(null, artist, song, tabType, contents);
             }
 
@@ -70,5 +71,44 @@ namespace UltimateGuitar
         }
 
         #endregion
+
+        private static readonly Regex NewlineRegex = new Regex("(?<!\r)\n", RegexOptions.Compiled);
+
+        private static string ConvertNewlines(string source)
+        {
+            return NewlineRegex.Replace(source, Environment.NewLine);
+        }
+
+        private static string StripHTML(string source)
+        {
+            var array = new char[source.Length];
+            var arrayIndex = 0;
+            var inside = false;
+
+            for (var i = 0; i < source.Length; i++)
+            {
+                var let = source[i];
+
+                if (let == '<')
+                {
+                    inside = true;
+                    continue;
+                }
+
+                if (let == '>')
+                {
+                    inside = false;
+                    continue;
+                }
+
+                if (!inside)
+                {
+                    array[arrayIndex] = let;
+                    arrayIndex++;
+                }
+            }
+
+            return new string(array, 0, arrayIndex);
+        }
     }
 }
