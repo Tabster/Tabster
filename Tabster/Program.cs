f8895d14abe33fe5cd5207d6048fe87a84abefea
Nate Shoffner <nate.shoffner@gmail.com>
2014-07-22 16:18:47 -0400

    Added error logging.

--- Tabster/Program.cs
+++ Tabster/Program.cs
@@ -2,7 +2,10 @@
 
 using System;
 using System.IO;
+using System.Reflection;
+using System.Text;
 using System.Windows.Forms;
+using Tabster.Plugins;
 
 #endregion
 
@@ -18,6 +21,8 @@ namespace Tabster
         [STAThread]
         public static void Main(string[] args)
         {
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
             var pluginDirectory = Path.Combine(Path.GetDirectoryName(Application.ExecutablePath), "Plugins");
 
             pluginController = new PluginController(pluginDirectory);
@@ -32,6 +37,41 @@ namespace Tabster
             TabHandler = new TabViewerManager();
 
             instanceController.Run(args);
+        }
+
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var exception = ((Exception)e.ExceptionObject).GetBaseException();
+
+            var assembly = Assembly.GetExecutingAssembly();
+
+            var sb = new StringBuilder();
+
+            sb.AppendLine("---- State Data ----");
+            sb.AppendLine(string.Format("Date/Time: {0}", DateTime.Now.ToString()));
+            sb.AppendLine(string.Format("Platform: {0}", Environment.OSVersion));
+            sb.AppendLine(string.Format("CLR Version: {0}", Environment.Version));
+            sb.AppendLine();
+
+            sb.AppendLine("---- Assembly Data ----");
+            sb.AppendLine(string.Format("Assembly Version: {0}", assembly.GetName().Version));
+            sb.AppendLine(string.Format("Assembly Full Name: {0}", assembly.FullName));
+            sb.AppendLine(string.Format("Assembly Path: {0}", assembly.Location));
+            sb.AppendLine();
+
+            sb.AppendLine("---- Exception Data ----");
+            sb.AppendLine(string.Format("Exception Message: {0}", exception.Message));
+            sb.AppendLine(string.Format("Exception Type: {0}", exception.GetType()));
+            sb.AppendLine(string.Format("Exception Source: {0}", exception.Source));
+            sb.AppendLine();
+
+            sb.AppendLine("---- Stack Trace ----");
+            sb.AppendLine(exception.StackTrace);
+
+            sb.AppendLine();
+            sb.AppendLine();
+
+            File.AppendAllText(Path.Combine(libraryManager.ApplicationDirectory, "error.log"), sb.ToString());
         }
     }
 }
