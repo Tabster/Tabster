aae24f69e1dfb97815647126c39050a1a3241547
Nate Shoffner <nate.shoffner@gmail.com>
2014-08-02 06:10:48 -0400

    Disable root node highlighting.

--- Tabster/Controls/StaticTreeView.cs
+++ Tabster/Controls/StaticTreeView.cs
@@ -13,14 +13,16 @@ namespace Tabster.Controls
         private const int TVM_SETEXTENDEDSTYLE = 0x1100 + 44;
         private const int TVS_EX_DOUBLEBUFFER = 0x0004;
 
+        private const int WM_LBUTTONDBLCLK = 0x0203; //515
+        private const int WM_LBUTTONDOWN = 0x0201; //513
+        private const int WM_LBUTTONUP = 0x0202; //514
+
         public bool AllowRootNodeSelection { get; set; }
+        public bool AutoSelectChildNode { get; set; }
 
         public TreeNode FirstNode
         {
-            get
-            {
-                return Nodes.Count > 0 ? Nodes[0] : null;
-            }
+            get { return Nodes.Count > 0 ? Nodes[0] : null; }
         }
 
         [DllImport("user32.dll")]
@@ -76,6 +78,35 @@ namespace Tabster.Controls
 
             if (nextNode != null)
                 SelectedNode = nextNode;
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == WM_LBUTTONDOWN || m.Msg == WM_LBUTTONUP || m.Msg == WM_LBUTTONDBLCLK)
+            {
+                var x = (Int16) m.LParam;
+                var y = (Int16) ((int) m.LParam >> 16);
+
+                var info = HitTest(x, y);
+
+                if (info.Node != null)
+                {
+                    if (info.Node.Parent == null)
+                    {
+                        if (AutoSelectChildNode && info.Node.Nodes.Count > 0)
+                        {
+                            SelectedNode = info.Node.FirstNode;
+                        }
+
+                        else
+                        {
+                            return;
+                        }
+                    }
+                }
+            }
+
+            base.WndProc(ref m);
         }
 
         #region Overrides
