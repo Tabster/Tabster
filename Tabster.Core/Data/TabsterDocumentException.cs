13d8d50831dca3c555dcf028ad298be4a1419696
Nate Shoffner <nate.shoffner@gmail.com>
2014-08-16 05:08:10 -0400

    Added TabsterDocumentException.cs.

---
+++ Tabster.Core/Data/TabsterDocumentException.cs
@@ -0,0 +1,20 @@
+﻿#region
+
+using System;
+
+#endregion
+
+namespace Tabster.Core.Data
+{
+    public class TabsterDocumentException : Exception
+    {
+        public TabsterDocumentException()
+        {
+        }
+
+        public TabsterDocumentException(string message)
+            : base(message)
+        {
+        }
+    }
+}
