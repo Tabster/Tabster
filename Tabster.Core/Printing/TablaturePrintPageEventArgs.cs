b6e64cb667edd00bb583f3d1e55978c6c00b4b6d
Nate Shoffner <nate.shoffner@gmail.com>
2014-08-16 18:21:13 -0400

    Added TablaturePrintPageEventArgs.cs.

---
+++ Tabster.Core/Printing/TablaturePrintPageEventArgs.cs
@@ -0,0 +1,19 @@
+﻿#region
+
+using System.Drawing;
+using System.Drawing.Printing;
+
+#endregion
+
+namespace Tabster.Core.Printing
+{
+    public class TablaturePrintPageEventArgs : PrintPageEventArgs
+    {
+        public TablaturePrintPageEventArgs(Graphics graphics, Rectangle marginBounds, Rectangle pageBounds, PageSettings pageSettings) :
+            base(graphics, marginBounds, pageBounds, pageSettings)
+        {
+        }
+
+        public int CurrentPage { get; set; }
+    }
+}
