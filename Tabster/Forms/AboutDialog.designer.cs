315958e1102f48f381549c0d7a0e2974d3a6c1ce
Nate Shoffner <nate.shoffner@gmail.com>
2014-08-20 19:33:47 -0400

    AboutDialog redesign.

--- Tabster/Forms/AboutDialog.designer.cs
+++ Tabster/Forms/AboutDialog.designer.cs
@@ -51,7 +51,7 @@
             this.lblName.BackColor = System.Drawing.Color.Transparent;
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblName.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblName.Location = new System.Drawing.Point(114, 9);
+            this.lblName.Location = new System.Drawing.Point(7, 9);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(103, 29);
             this.lblName.TabIndex = 40;
@@ -62,7 +62,7 @@
             this.lblCopyright.AutoSize = true;
             this.lblCopyright.BackColor = System.Drawing.Color.Transparent;
             this.lblCopyright.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lblCopyright.Location = new System.Drawing.Point(116, 47);
+            this.lblCopyright.Location = new System.Drawing.Point(9, 47);
             this.lblCopyright.Name = "lblCopyright";
             this.lblCopyright.Size = new System.Drawing.Size(51, 13);
             this.lblCopyright.TabIndex = 47;
@@ -72,7 +72,7 @@
             // 
             this.closebtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.closebtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.closebtn.Location = new System.Drawing.Point(344, 313);
+            this.closebtn.Location = new System.Drawing.Point(344, 285);
             this.closebtn.Name = "closebtn";
             this.closebtn.Size = new System.Drawing.Size(75, 23);
             this.closebtn.TabIndex = 48;
@@ -93,7 +93,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::Tabster.Properties.Resources.Guitar256;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(323, 9);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(96, 96);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -106,7 +106,7 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
             this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
-            this.linkLabel1.Location = new System.Drawing.Point(184, 73);
+            this.linkLabel1.Location = new System.Drawing.Point(77, 70);
             this.linkLabel1.Name = "linkLabel1";
             this.linkLabel1.Size = new System.Drawing.Size(120, 13);
             this.linkLabel1.TabIndex = 54;
@@ -161,7 +161,7 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label5.Location = new System.Drawing.Point(116, 73);
+            this.label5.Location = new System.Drawing.Point(9, 70);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(62, 13);
             this.label5.TabIndex = 60;
@@ -173,7 +173,7 @@
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.linkLabel3);
             this.groupBox1.Controls.Add(this.linkLabel2);
-            this.groupBox1.Location = new System.Drawing.Point(12, 235);
+            this.groupBox1.Location = new System.Drawing.Point(12, 210);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(299, 68);
             this.groupBox1.TabIndex = 61;
@@ -182,7 +182,7 @@
             // 
             // txtLicense
             // 
-            this.txtLicense.Location = new System.Drawing.Point(12, 136);
+            this.txtLicense.Location = new System.Drawing.Point(12, 111);
             this.txtLicense.Multiline = true;
             this.txtLicense.Name = "txtLicense";
             this.txtLicense.ReadOnly = true;
@@ -195,7 +195,7 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label2.Location = new System.Drawing.Point(9, 120);
+            this.label2.Location = new System.Drawing.Point(9, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 13);
             this.label2.TabIndex = 63;
@@ -207,7 +207,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.CancelButton = this.closebtn;
-            this.ClientSize = new System.Drawing.Size(431, 348);
+            this.ClientSize = new System.Drawing.Size(431, 320);
             this.ControlBox = false;
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtLicense);
