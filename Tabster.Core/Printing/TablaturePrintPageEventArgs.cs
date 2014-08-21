41494e2f7f296808872ee47b3fe885e4a2d35fc6
Nate Shoffner <nate.shoffner@gmail.com>
2014-08-21 00:17:22 -0400

    Added TablaturePrintPageEventArgs.cs.

--- Tabster.Core/Printing/TablaturePrintPageEventArgs.cs
+++ Tabster.Core/Printing/TablaturePrintPageEventArgs.cs
@@ -14,6 +14,14 @@ namespace Tabster.Core.Printing
         {
         }
 
+        /// <summary>
+        /// The current page number being printed.
+        /// </summary>
         public int CurrentPage { get; set; }
+
+        /// <summary>
+        /// Real page bounds based on printable area of the page.
+        /// </summary>
+        public Rectangle RealPageBounds { get; set; }
     }
 }
