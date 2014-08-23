99b35960baa7b8fc30f467cd144cf97efc9ee0c6
Nate Shoffner <nate.shoffner@gmail.com>
2014-08-23 13:29:57 -0400

    Added TabRatingExtensions.

--- Tabster.Core/Types/TabRating.cs
+++ Tabster.Core/Types/TabRating.cs
@@ -1,4 +1,6 @@
-﻿namespace Tabster.Core.Types
+﻿using System;
+
+namespace Tabster.Core.Types
 {
     /// <summary>
     ///   Tab rating.
@@ -11,5 +13,29 @@
         Stars3 = 3,
         Stars4 = 4,
         Stars5 = 5
+    }
+
+    public static class TabRatingExtensions
+    {
+        public static int ToInt(this TabRating rating)
+        {
+            switch(rating)
+            {
+                case TabRating.None:
+                    return 0;
+                case TabRating.Stars1:
+                    return 1;
+                case TabRating.Stars2:
+                    return 2;
+                case TabRating.Stars3:
+                    return 3;
+                case TabRating.Stars4:
+                    return 4;
+                case TabRating.Stars5:
+                    return 5;
+                default:
+                    throw new ArgumentOutOfRangeException("rating");
+            }
+        }
     }
 }
