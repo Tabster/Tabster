e8533e01d689be06d7b9b164dca83f15ebe72846
Nate Shoffner <nate.shoffner@gmail.com>
2014-08-23 01:01:15 -0400

    Move copyright text.

--- Tabster/Forms/SplashScreen.designer.cs
+++ Tabster/Forms/SplashScreen.designer.cs
@@ -81,7 +81,7 @@ namespace Tabster.Forms
             this.lblCopyright.BackColor = System.Drawing.Color.Transparent;
             this.lblCopyright.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCopyright.ForeColor = System.Drawing.Color.Gray;
-            this.lblCopyright.Location = new System.Drawing.Point(179, 153);
+            this.lblCopyright.Location = new System.Drawing.Point(108, 153);
             this.lblCopyright.Name = "lblCopyright";
             this.lblCopyright.Size = new System.Drawing.Size(65, 16);
             this.lblCopyright.TabIndex = 48;
