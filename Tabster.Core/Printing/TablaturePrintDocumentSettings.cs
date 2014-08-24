bdd202c28bec8651f02fc5d075e56e1b6c65d6e3
Nate Shoffner <nate.shoffner@gmail.com>
2014-08-23 20:27:32 -0400

    Added PrintColor property to TablaturePrintDocumentSettings.

--- Tabster.Core/Printing/TablaturePrintDocumentSettings.cs
+++ Tabster.Core/Printing/TablaturePrintDocumentSettings.cs
@@ -1,9 +1,22 @@
-﻿namespace Tabster.Core.Printing
+﻿#region
+
+using System.Drawing;
+
+#endregion
+
+namespace Tabster.Core.Printing
 {
     public sealed class TablaturePrintDocumentSettings
     {
+        public TablaturePrintDocumentSettings()
+        {
+            PrintColor = Brushes.Black;
+        }
+
         public bool DisplayTitle { get; set; }
         public bool DisplayPageNumbers { get; set; }
         public bool DisplayPrintTime { get; set; }
+
+        public Brush PrintColor { get; set; }
     }
 }
