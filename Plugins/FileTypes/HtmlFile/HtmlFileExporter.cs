b4af27e1865b06102c51cbcffb6a9617b51250d3
Nate Shoffner <nate.shoffner@gmail.com>
2014-09-18 12:05:46 -0400

    Html file export project.

---
+++ Plugins/FileTypes/HtmlFile/HtmlFileExporter.cs
@@ -0,0 +1,47 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.IO;
+using HtmlFile.Properties;
+using Tabster.Core.Data;
+using Tabster.Core.Data.Processing;
+using Tabster.Core.Types;
+
+#endregion
+
+namespace HtmlFile
+{
+    public class HtmlFileExporter : ITablatureFileExporter
+    {
+        public HtmlFileExporter()
+        {
+            FileType = new FileType("HTML File", new[] {".html", ".htm"});
+        }
+
+        #region Implementation of ITablatureFileExporter
+
+        public FileType FileType { get; private set; }
+
+        public void Export(TablatureDocument doc, string fileName)
+        {
+            var templates = new Dictionary<string, string>
+                                {
+                                    {"{TAB_ARTIST}", doc.Artist},
+                                    {"{TAB_TITLE}", doc.Title},
+                                    {"{TAB_TYPE}", doc.Type.ToFriendlyString()},
+                                    {"{TAB_CONTENTS}", doc.Contents}
+                                };
+
+            var html = Resources.Html_Template;
+
+            foreach (var templatePair in templates)
+            {
+                html = html.Replace(templatePair.Key, templatePair.Value);
+            }
+
+            File.WriteAllText(fileName, html);
+        }
+
+        #endregion
+    }
+}
---
+++ Plugins/FileTypes/HtmlFile/HtmlFilePlugin.cs
@@ -0,0 +1,51 @@
+﻿#region
+
+using System;
+using Tabster.Core.Plugins;
+
+#endregion
+
+namespace HtmlFile
+{
+    public class HtmlFilePlugin : ITabsterPlugin
+    {
+        #region Implementation of ITabsterPlugin
+
+        public string Author
+        {
+            get { return "Nate Shoffner"; }
+        }
+
+        public string Copyright
+        {
+            get { return "Copyright © Nate Shoffner 2014"; }
+        }
+
+        public string Description
+        {
+            get { return "Supports exporting to HTML files. "; }
+        }
+
+        public string DisplayName
+        {
+            get { return "HTML file support"; }
+        }
+
+        public Version Version
+        {
+            get { return new Version("1.0"); }
+        }
+
+        public Uri Website
+        {
+            get { return new Uri("http://nateshoffner.com"); }
+        }
+
+        public Type[] Types
+        {
+            get { return new[] {typeof (HtmlFileExporter)}; }
+        }
+
+        #endregion
+    }
+}
