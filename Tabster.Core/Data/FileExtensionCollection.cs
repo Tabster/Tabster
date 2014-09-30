bea23aa77a152d63f541fdf60efd05bdbcefff9e
Nate Shoffner <nate.shoffner@gmail.com>
2014-09-29 23:40:49 -0400

    Implement CustomTypeCollection<T>.

--- Tabster.Core/Data/FileExtensionCollection.cs
+++ Tabster.Core/Data/FileExtensionCollection.cs
@@ -1,96 +1,21 @@
 #region
 
-using System;
-using System.Collections;
 using System.Collections.Generic;
+using Tabster.Core.Utilities;
 
 #endregion
 
 namespace Tabster.Core.Data
 {
-    public sealed class FileExtensionCollection : ICollection<string>
+    public sealed class FileExtensionCollection : CustomTypeCollection<string>
     {
-        private readonly List<string> _extensions = new List<string>();
-
-        public FileExtensionCollection(IEnumerable<string> extensions)
+        public FileExtensionCollection(IEnumerable<string> extensions) : base(extensions)
         {
-            _extensions = new List<string>(extensions);
         }
 
         public FileExtensionCollection(string extension)
             : this(new[] {extension})
         {
-        }
-
-        #region Implementation of IEnumerable
-
-        public IEnumerator<string> GetEnumerator()
-        {
-            foreach (var extension in _extensions)
-            {
-                yield return extension;
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        #endregion
-
-        #region Implementation of ICollection<string>
-
-        public void Add(string extension)
-        {
-            _extensions.Add(extension);
-        }
-
-        public void Clear()
-        {
-            _extensions.Clear();
-        }
-
-        public bool Contains(string extension)
-        {
-            return _extensions.Contains(extension);
-        }
-
-        public void CopyTo(string[] array, int arrayIndex)
-        {
-            _extensions.CopyTo(array, arrayIndex);
-        }
-
-        public bool Remove(string extension)
-        {
-            return _extensions.Remove(extension);
-        }
-
-        public int Count
-        {
-            get { return _extensions.Count; }
-        }
-
-        public bool IsReadOnly
-        {
-            get { return false; }
-        }
-
-        #endregion
-
-        public string this[int index]
-        {
-            get { return _extensions[index]; }
-        }
-
-        public bool Contains(string extension, StringComparison comparison)
-        {
-            return _extensions.Find(x => x.Equals(extension, comparison)) != null;
-        }
-
-        public string[] ToArray()
-        {
-            return _extensions.ToArray();
         }
     }
 }
---
+++ Tabster.Core/Utilities/CustomTypeCollection.cs
@@ -0,0 +1,96 @@
+﻿#region
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+#endregion
+
+namespace Tabster.Core.Utilities
+{
+    public class CustomTypeCollection<T> : ICollection<T> where T : class
+    {
+        private readonly List<T> _items = new List<T>();
+
+        internal CustomTypeCollection(IEnumerable<T> items)
+        {
+            _items = new List<T>(items);
+        }
+
+        internal CustomTypeCollection(T item)
+            : this(new[] {item})
+        {
+        }
+
+        #region Implementation of IEnumerable
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            foreach (var item in _items)
+            {
+                yield return item;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        #endregion
+
+        #region Implementation of ICollection<T>
+
+        public void Add(T item)
+        {
+            _items.Add(item);
+        }
+
+        public void Clear()
+        {
+            _items.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return _items.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            _items.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(T item)
+        {
+            return _items.Remove(item);
+        }
+
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return false; }
+        }
+
+        #endregion
+
+        public T this[int index]
+        {
+            get { return _items[index]; }
+        }
+
+        public bool Contains(T item, StringComparison comparison)
+        {
+            return _items.Find(x => Equals(item, comparison)) != null;
+        }
+
+        public T[] ToArray()
+        {
+            return _items.ToArray();
+        }
+    }
+}
