a54db12f1df33f4d55f6c6f74dd1ceca19e24382
Nate Shoffner <nate.shoffner@gmail.com>
2014-10-19 16:41:29 -0400

    Microsoft Word Document (.docx) plugin.

---
+++ Plugins/FileTypes/WordDoc/WordDocExporter.cs
@@ -0,0 +1,35 @@
+﻿#region
+
+using System.Drawing;
+using Novacode;
+using Tabster.Data;
+using Tabster.Data.Processing;
+
+#endregion
+
+namespace WordDoc
+{
+    public class WordDocExporter : ITablatureFileExporter
+    {
+        public WordDocExporter()
+        {
+            FileType = new FileType("Microsoft Office Open XML Format Document", ".docx");
+        }
+
+        #region Implementation of ITablatureFileExporter
+
+        public FileType FileType { get; private set; }
+
+        public void Export(TablatureDocument doc, string fileName)
+        {
+            using (var document = DocX.Create(fileName))
+            {
+                var p = document.InsertParagraph();
+                p.Append(doc.Contents).Font(new FontFamily("Courier New")).FontSize(9);
+                document.Save();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Plugins/FileTypes/WordDoc/WordDocImporter.cs
@@ -0,0 +1,52 @@
+#region
+
+using System.Text;
+using Novacode;
+using Tabster.Core.Types;
+using Tabster.Data;
+using Tabster.Data.Processing;
+
+#endregion
+
+namespace WordDoc
+{
+    public class WordDocImporter : ITablatureFileImporter
+    {
+        public WordDocImporter()
+        {
+            FileType = new FileType("Microsoft Office Open XML Format Document", ".docx");
+        }
+
+        #region Implementation of ITablatureDocumentImporter
+
+        public FileType FileType { get; private set; }
+
+        public TablatureDocument Import(string fileName)
+        {
+            using (var document = DocX.Load(fileName))
+            {
+                var sb = new StringBuilder();
+
+                foreach (var paragraph in document.Paragraphs)
+                {
+                    sb.AppendLine(paragraph.Text);
+                    sb.AppendLine();
+                }
+
+                var doc = new TablatureDocument {Contents = sb.ToString()};
+                return doc;
+            }
+        }
+
+        public TablatureDocument Import(string fileName, string artist, string title, TablatureType type)
+        {
+            var doc = Import(fileName);
+            doc.Artist = artist;
+            doc.Title = title;
+            doc.Type = type;
+            return doc;
+        }
+
+        #endregion
+    }
+}
