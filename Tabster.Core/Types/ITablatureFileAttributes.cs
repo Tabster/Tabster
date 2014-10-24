f08e27805ebdf575d63e08bed59f18d46614534c
Nate Shoffner <nate.shoffner@gmail.com>
2014-10-24 14:13:13 -0400

    Rename ITablatureUserDefined to ITablatureFileAttributes.

--- Tabster.Core/Types/ITablatureFileAttributes.cs
+++ Tabster.Core/Types/ITablatureFileAttributes.cs
@@ -6,7 +6,7 @@ using System;
 
 namespace Tabster.Core.Types
 {
-    public interface ITablatureUserDefined : ITablatureAttributes
+    public interface ITablatureFileAttributes : ITablatureAttributes
     {
         string Comment { get; set; }
         DateTime Created { get; set; }
