bf06f6c19299ba63779d53202ca01b55e68fd514
Nate Shoffner <nate.shoffner@gmail.com>
2014-10-26 02:49:15 -0400

    Remove HTMLAgilityPack credit.

--- Tabster/Forms/AboutDialog.designer.cs
+++ Tabster/Forms/AboutDialog.designer.cs
@@ -31,11 +31,9 @@
             this.lblName = new System.Windows.Forms.Label();
             this.lblCopyright = new System.Windows.Forms.Label();
             this.closebtn = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.label4 = new System.Windows.Forms.Label();
-            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             this.linkLabel3 = new System.Windows.Forms.LinkLabel();
             this.label5 = new System.Windows.Forms.Label();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
@@ -71,23 +69,12 @@
             // 
             this.closebtn.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.closebtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.closebtn.Location = new System.Drawing.Point(344, 285);
+            this.closebtn.Location = new System.Drawing.Point(344, 227);
             this.closebtn.Name = "closebtn";
             this.closebtn.Size = new System.Drawing.Size(75, 23);
             this.closebtn.TabIndex = 48;
             this.closebtn.Text = "Close";
             this.closebtn.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.Transparent;
-            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label1.Location = new System.Drawing.Point(6, 17);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(98, 13);
-            this.label1.TabIndex = 52;
-            this.label1.Text = "HTML Agility Pack:";
             // 
             // pictureBox1
             // 
@@ -118,26 +105,11 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.label4.Location = new System.Drawing.Point(6, 37);
+            this.label4.Location = new System.Drawing.Point(6, 16);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(75, 13);
             this.label4.TabIndex = 57;
             this.label4.Text = "FatCow Icons:";
-            // 
-            // linkLabel2
-            // 
-            this.linkLabel2.ActiveLinkColor = System.Drawing.Color.Blue;
-            this.linkLabel2.AutoSize = true;
-            this.linkLabel2.BackColor = System.Drawing.Color.Transparent;
-            this.linkLabel2.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
-            this.linkLabel2.Location = new System.Drawing.Point(110, 17);
-            this.linkLabel2.Name = "linkLabel2";
-            this.linkLabel2.Size = new System.Drawing.Size(176, 13);
-            this.linkLabel2.TabIndex = 58;
-            this.linkLabel2.TabStop = true;
-            this.linkLabel2.Text = "http://htmlagilitypack.codeplex.com";
-            this.linkLabel2.VisitedLinkColor = System.Drawing.Color.Blue;
-            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkClicked);
             // 
             // linkLabel3
             // 
@@ -145,7 +117,7 @@
             this.linkLabel3.AutoSize = true;
             this.linkLabel3.BackColor = System.Drawing.Color.Transparent;
             this.linkLabel3.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
-            this.linkLabel3.Location = new System.Drawing.Point(110, 37);
+            this.linkLabel3.Location = new System.Drawing.Point(110, 16);
             this.linkLabel3.Name = "linkLabel3";
             this.linkLabel3.Size = new System.Drawing.Size(120, 13);
             this.linkLabel3.TabIndex = 59;
@@ -167,13 +139,11 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.label4);
             this.groupBox1.Controls.Add(this.linkLabel3);
-            this.groupBox1.Controls.Add(this.linkLabel2);
             this.groupBox1.Location = new System.Drawing.Point(12, 210);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(299, 68);
+            this.groupBox1.Size = new System.Drawing.Size(299, 40);
             this.groupBox1.TabIndex = 61;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Special Thanks:";
@@ -194,7 +164,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Control;
             this.CancelButton = this.closebtn;
-            this.ClientSize = new System.Drawing.Size(431, 320);
+            this.ClientSize = new System.Drawing.Size(431, 259);
             this.ControlBox = false;
             this.Controls.Add(this.txtLicense);
             this.Controls.Add(this.groupBox1);
@@ -226,11 +196,9 @@
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Label lblCopyright;
         private System.Windows.Forms.Button closebtn;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.LinkLabel linkLabel1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.LinkLabel linkLabel2;
         private System.Windows.Forms.LinkLabel linkLabel3;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.GroupBox groupBox1;
