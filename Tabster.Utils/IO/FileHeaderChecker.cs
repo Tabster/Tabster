a860267fbe480a046e16d6f1d85a9f08eff9979b
Nate Shoffner <nate.shoffner@gmail.com>
2014-10-26 04:49:35 -0400

    Added FileHeaderChecker to Tabster.Utils.

---
+++ Tabster.Utils/IO/FileHeaderChecker.cs
@@ -0,0 +1,34 @@
+﻿#region
+
+using System.IO;
+using System.Linq;
+
+#endregion
+
+namespace Tabster.Utilities.IO
+{
+    public static class FileHeaderChecker
+    {
+        public static bool Check(string fileName, byte[] header, int startIndex = 0, bool throwOnMissing = false)
+        {
+            try
+            {
+                using (var fs = new FileStream(fileName, FileMode.Open))
+                {
+                    var buffer = new byte[header.Length];
+                    fs.Read(buffer, startIndex, header.Length);
+
+                    return buffer.SequenceEqual(header);
+                }
+            }
+
+            catch (FileNotFoundException)
+            {
+                if (throwOnMissing)
+                    throw;
+
+                return false;
+            }
+        }
+    }
+}
