1b21cf0f134de7f924d19198a4e145a1d1ba31c7
Nate Shoffner <nate.shoffner@gmail.com>
2014-11-10 01:45:38 -0500

    Added Title property to TablaturePrintDocumentSettings.

--- Tabster.Core/Printing/TablaturePrintDocumentSettings.cs
+++ Tabster.Core/Printing/TablaturePrintDocumentSettings.cs
@@ -13,6 +13,8 @@ namespace Tabster.Core.Printing
             PrintColor = Brushes.Black;
         }
 
+        public string Title { get; set; }
+
         public bool DisplayTitle { get; set; }
         public bool DisplayPageNumbers { get; set; }
         public bool DisplayPrintTime { get; set; }
