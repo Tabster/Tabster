f5fd0c2fd1642738d148cfff02b8394bc88b87e2
Nate Shoffner <nate.shoffner@gmail.com>
2014-12-15 15:53:14 -0500

    Moved batch downloader to separate window.

---
+++ Tabster/Forms/DownloadDialog.Designer.cs
@@ -0,0 +1,326 @@
+﻿namespace Tabster.Forms
+{
+    partial class DownloadDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtUrls = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listDownloads = new System.Windows.Forms.ListView();
+            this.colUrl = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.colStatus = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.DownloadBackgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnLibrary = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.typeList = new Tabster.Controls.TabTypeDropdown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtArtist = new System.Windows.Forms.TextBox();
+            this.btnUrls = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox1.SuspendLayout();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtUrls
+            // 
+            this.txtUrls.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtUrls.Location = new System.Drawing.Point(3, 19);
+            this.txtUrls.Multiline = true;
+            this.txtUrls.Name = "txtUrls";
+            this.txtUrls.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtUrls.Size = new System.Drawing.Size(426, 138);
+            this.txtUrls.TabIndex = 12;
+            this.txtUrls.TextChanged += new System.EventHandler(this.txtUrls_TextChanged);
+            this.txtUrls.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtUrls_KeyDown);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnDownload.Enabled = false;
+            this.btnDownload.Location = new System.Drawing.Point(302, 163);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(127, 23);
+            this.btnDownload.TabIndex = 20;
+            this.btnDownload.Text = "Begin Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(190, 13);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Enter URLs (one per line) to download:";
+            // 
+            // listDownloads
+            // 
+            this.listDownloads.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listDownloads.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.colUrl,
+            this.colStatus});
+            this.listDownloads.FullRowSelect = true;
+            this.listDownloads.GridLines = true;
+            this.listDownloads.Location = new System.Drawing.Point(3, 19);
+            this.listDownloads.MultiSelect = false;
+            this.listDownloads.Name = "listDownloads";
+            this.listDownloads.Size = new System.Drawing.Size(426, 125);
+            this.listDownloads.TabIndex = 22;
+            this.listDownloads.UseCompatibleStateImageBehavior = false;
+            this.listDownloads.View = System.Windows.Forms.View.Details;
+            this.listDownloads.SelectedIndexChanged += new System.EventHandler(this.listDownloads_SelectedIndexChanged);
+            // 
+            // colUrl
+            // 
+            this.colUrl.Text = "URL";
+            this.colUrl.Width = 285;
+            // 
+            // colStatus
+            // 
+            this.colStatus.Text = "Status";
+            this.colStatus.Width = 132;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(136, 169);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(160, 10);
+            this.progressBar1.TabIndex = 23;
+            this.progressBar1.Visible = false;
+            // 
+            // DownloadBackgroundWorker
+            // 
+            this.DownloadBackgroundWorker.WorkerReportsProgress = true;
+            this.DownloadBackgroundWorker.WorkerSupportsCancellation = true;
+            this.DownloadBackgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.DownloadBackgroundWorker_DoWork);
+            this.DownloadBackgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.DownloadBackgroundWorker_ProgressChanged);
+            this.DownloadBackgroundWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.DownloadBackgroundWorker_RunWorkerCompleted);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnCancel.Location = new System.Drawing.Point(3, 163);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(127, 23);
+            this.btnCancel.TabIndex = 25;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnLibrary
+            // 
+            this.btnLibrary.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnLibrary.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.btnLibrary.Location = new System.Drawing.Point(302, 163);
+            this.btnLibrary.Name = "btnLibrary";
+            this.btnLibrary.Size = new System.Drawing.Size(127, 23);
+            this.btnLibrary.TabIndex = 26;
+            this.btnLibrary.Text = "Add tabs to Library";
+            this.btnLibrary.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnUpdate);
+            this.groupBox1.Controls.Add(this.typeList);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtTitle);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtArtist);
+            this.groupBox1.Location = new System.Drawing.Point(7, 19);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 134);
+            this.groupBox1.TabIndex = 27;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tab Details";
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(47, 105);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(92, 23);
+            this.btnUpdate.TabIndex = 28;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // typeList
+            // 
+            this.typeList.DefaultText = "";
+            this.typeList.DisplayDefault = false;
+            this.typeList.Location = new System.Drawing.Point(47, 75);
+            this.typeList.Name = "typeList";
+            this.typeList.Size = new System.Drawing.Size(119, 21);
+            this.typeList.TabIndex = 22;
+            this.typeList.UsePluralizedNames = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(5, 78);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(34, 13);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Type:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(9, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Title:";
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.BackColor = System.Drawing.SystemColors.Window;
+            this.txtTitle.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtTitle.Location = new System.Drawing.Point(47, 49);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(200, 20);
+            this.txtTitle.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(6, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Artist:";
+            // 
+            // txtArtist
+            // 
+            this.txtArtist.BackColor = System.Drawing.SystemColors.Window;
+            this.txtArtist.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtArtist.Location = new System.Drawing.Point(47, 23);
+            this.txtArtist.Name = "txtArtist";
+            this.txtArtist.Size = new System.Drawing.Size(200, 20);
+            this.txtArtist.TabIndex = 17;
+            // 
+            // btnUrls
+            // 
+            this.btnUrls.Location = new System.Drawing.Point(3, 163);
+            this.btnUrls.Name = "btnUrls";
+            this.btnUrls.Size = new System.Drawing.Size(127, 23);
+            this.btnUrls.TabIndex = 24;
+            this.btnUrls.Text = "Add URLs";
+            this.btnUrls.UseVisualStyleBackColor = true;
+            this.btnUrls.Click += new System.EventHandler(this.btnUrls_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(12, 12);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.label4);
+            this.splitContainer1.Panel1.Controls.Add(this.progressBar1);
+            this.splitContainer1.Panel1.Controls.Add(this.btnCancel);
+            this.splitContainer1.Panel1.Controls.Add(this.btnUrls);
+            this.splitContainer1.Panel1.Controls.Add(this.txtUrls);
+            this.splitContainer1.Panel1.Controls.Add(this.listDownloads);
+            this.splitContainer1.Panel1.Controls.Add(this.btnDownload);
+            this.splitContainer1.Panel1.Controls.Add(this.btnLibrary);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
+            this.splitContainer1.Size = new System.Drawing.Size(699, 193);
+            this.splitContainer1.SplitterDistance = 432;
+            this.splitContainer1.TabIndex = 28;
+            // 
+            // DownloadDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 211);
+            this.Controls.Add(this.splitContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DownloadDialog";
+            this.Text = "Batch Download";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.DownloadDialog_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtUrls;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListView listDownloads;
+        private System.Windows.Forms.ColumnHeader colUrl;
+        private System.Windows.Forms.ColumnHeader colStatus;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker DownloadBackgroundWorker;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnLibrary;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Controls.TabTypeDropdown typeList;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.TextBox txtTitle;
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.TextBox txtArtist;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnUrls;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+
+    }
+}
---
+++ Tabster/Forms/DownloadDialog.cs
@@ -0,0 +1,296 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+using Tabster.Core.Types;
+using Tabster.Data;
+using Tabster.Data.Processing;
+using Tabster.Utilities.Net;
+
+#endregion
+
+namespace Tabster.Forms
+{
+    public partial class DownloadDialog : Form
+    {
+        public enum DownloadState
+        {
+            MissingParser,
+            Cancelled,
+            Completed,
+            Failed
+        }
+
+        #region Nested type: DownloadProcedure
+
+        internal class DownloadProcedure
+        {
+            public DownloadProcedure(Uri url)
+            {
+                Url = url;
+            }
+
+            public Uri Url { get; private set; }
+            public ITablatureWebpageImporter Parser { get; set; }
+            public TablatureDocument Tab { get; set; }
+            public DownloadState State { get; set; }
+        }
+
+        #endregion
+
+        private readonly List<TablatureDocument> _downloadedTabs = new List<TablatureDocument>();
+        private readonly List<ITablatureWebpageImporter> _importers = new List<ITablatureWebpageImporter>();
+        private bool mClosePending;
+        private bool mCompleted = true;
+
+        public DownloadDialog(List<ITablatureWebpageImporter> importers)
+        {
+            InitializeComponent();
+            _importers = importers;
+
+            splitContainer1.Panel2Collapsed = true;
+        }
+
+        public ReadOnlyCollection<TablatureDocument> DownloadedTabs
+        {
+            get { return _downloadedTabs.AsReadOnly(); }
+        }
+
+        private Uri[] GetUrls()
+        {
+            var urls = new List<Uri>();
+
+            foreach (var line in txtUrls.Lines.Where(line => !string.IsNullOrEmpty(line)))
+            {
+                Uri url;
+                var validUrl = Uri.TryCreate(line, UriKind.Absolute, out url) &&
+                               (url.Scheme == Uri.UriSchemeHttp || url.Scheme == Uri.UriSchemeHttps);
+
+                if (validUrl)
+                {
+                    urls.Add(url);
+                }
+            }
+
+            return urls.ToArray();
+        }
+
+        private void txtUrls_TextChanged(object sender, EventArgs e)
+        {
+            btnDownload.Enabled = GetUrls().Length > 0;
+        }
+
+        private void PerformQueuedDownload(DownloadProcedure download)
+        {
+            using (var client = new TabsterWebClient(Program.CustomProxyController.GetProxy()))
+            {
+                try
+                {
+                    var src = client.DownloadString(download.Url);
+
+                    var tab = download.Parser.Parse(src, null);
+
+                    if (tab != null)
+                    {
+                        tab.Source = download.Url;
+                        tab.SourceType = TablatureSourceType.Download;
+                    }
+
+                    download.Tab = tab;
+
+                    download.State = DownloadState.Completed;
+                }
+
+                catch
+                {
+                    download.State = DownloadState.Failed;
+                }
+            }
+        }
+
+        private void DownloadBackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var queuedDownloads = (List<DownloadProcedure>) e.Argument;
+
+            var count = 0;
+
+            foreach (var queuedDownload in queuedDownloads)
+            {
+                if (DownloadBackgroundWorker.CancellationPending)
+                {
+                    queuedDownload.State = DownloadState.Cancelled;
+                }
+
+                else if (queuedDownload.Parser == null)
+                {
+                    queuedDownload.State = DownloadState.MissingParser;
+                }
+
+                else
+                {
+                    PerformQueuedDownload(queuedDownload);
+                }
+
+                count++;
+                DownloadBackgroundWorker.ReportProgress(count, queuedDownload);
+            }
+
+            e.Result = queuedDownloads;
+        }
+
+        private void DownloadBackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+
+            var download = (DownloadProcedure) e.UserState;
+
+            if (download.State == DownloadState.Completed)
+                _downloadedTabs.Add(download.Tab);
+
+            var lvi = listDownloads.Items[_downloadedTabs.Count - 1];
+            lvi.SubItems[colStatus.Index].Text =
+                GetDownloadStateString(download.State);
+
+            listDownloads.EnsureVisible(lvi.Index);
+        }
+
+        private void DownloadBackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            mCompleted = true;
+            if (mClosePending)
+                Close();
+
+            if (e.Error != null || e.Cancelled)
+                return;
+
+            progressBar1.Visible = false;
+            btnUrls.BringToFront();
+
+            if (listDownloads.Items.Count > 0)
+            {
+                btnLibrary.BringToFront();
+                listDownloads.Items[0].Selected = true;
+            }
+
+            splitContainer1.Panel2Collapsed = listDownloads.Items.Count == 0;
+        }
+
+        private static string GetDownloadStateString(DownloadState state)
+        {
+            switch (state)
+            {
+                case DownloadState.MissingParser:
+                    return "No suitable parser found";
+                case DownloadState.Cancelled:
+                    return "Cancelled";
+                case DownloadState.Completed:
+                    return "Completed";
+                case DownloadState.Failed:
+                    return "Failed";
+                default:
+                    throw new ArgumentOutOfRangeException("state");
+            }
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            DisplayDownloadList();
+
+            var urls = GetUrls();
+
+            txtUrls.Clear();
+
+            var queuedDownloads =
+                urls.Select(
+                    url => new DownloadProcedure(url) {Parser = _importers.Find(x => x.MatchesUrlPattern(url))})
+                    .ToList();
+
+            foreach (
+                var lvi in
+                    queuedDownloads.Select(queuedDownload => new ListViewItem {Text = queuedDownload.Url.ToString()}))
+            {
+                lvi.SubItems.Add("Idle");
+                listDownloads.Items.Add(lvi);
+            }
+
+            progressBar1.Maximum = queuedDownloads.Count;
+
+            btnCancel.BringToFront();
+            progressBar1.Visible = true;
+
+            mCompleted = false;
+
+            DownloadBackgroundWorker.RunWorkerAsync(queuedDownloads);
+        }
+
+        private void txtUrls_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == (Keys.Control | Keys.A))
+            {
+                txtUrls.SelectAll();
+                e.Handled = e.SuppressKeyPress = true;
+            }
+        }
+
+        private void DownloadDialog_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!mCompleted)
+            {
+                DownloadBackgroundWorker.CancelAsync();
+                Enabled = false;
+                e.Cancel = true;
+                mClosePending = true;
+            }
+        }
+
+        private void DisplayUrlBox()
+        {
+            label4.Visible = true;
+
+            txtUrls.BringToFront();
+            btnDownload.BringToFront();
+            btnCancel.BringToFront();
+        }
+
+        private void DisplayDownloadList()
+        {
+            label4.Visible = false;
+            listDownloads.BringToFront();
+        }
+
+        private void btnUrls_Click(object sender, EventArgs e)
+        {
+            DisplayUrlBox();
+        }
+
+        private void listDownloads_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            TablatureDocument tab = null;
+
+            if (listDownloads.SelectedItems.Count > 0)
+                tab = _downloadedTabs[listDownloads.SelectedItems[0].Index];
+
+            PopulateTablatureFields(tab);
+        }
+
+        private void PopulateTablatureFields(TablatureDocument tab)
+        {
+            txtArtist.Text = tab != null ? tab.Artist : string.Empty;
+            txtTitle.Text = tab != null ? tab.Title : string.Empty;
+            typeList.SelectedType = tab != null ? tab.Type : null;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var tab = _downloadedTabs[listDownloads.SelectedItems[0].Index];
+
+            tab.Artist = txtArtist.Text;
+            tab.Title = txtTitle.Text;
+            tab.Type = typeList.SelectedType;
+        }
+    }
+}
