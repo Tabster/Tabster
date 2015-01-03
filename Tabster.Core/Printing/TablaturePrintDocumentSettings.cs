fe82376d79cea7e67a1d169b53fb3c2d7f9420fc
Nate Shoffner <nate.shoffner@gmail.com>
2015-01-03 12:06:25 -0500

    Create TablaturePrintDocumentSettingsBase.

--- Tabster.Core/Printing/TablaturePrintDocumentSettings.cs
+++ Tabster.Core/Printing/TablaturePrintDocumentSettings.cs
@@ -1,24 +1,10 @@
-﻿#region
-
-using System.Drawing;
-
-#endregion
-
-namespace Tabster.Core.Printing
+﻿namespace Tabster.Core.Printing
 {
-    public sealed class TablaturePrintDocumentSettings
+    public sealed class TablaturePrintDocumentSettings : TablaturePrintDocumentSettingsBase
     {
-        public TablaturePrintDocumentSettings()
-        {
-            PrintColor = Color.Black;
-        }
-
         public string Title { get; set; }
 
         public bool DisplayTitle { get; set; }
-        public bool DisplayPageNumbers { get; set; }
         public bool DisplayPrintTime { get; set; }
-
-        public Color PrintColor { get; set; }
     }
 }
---
+++ Tabster.Core/Printing/TablaturePrintDocumentSettingsBase.cs
@@ -0,0 +1,20 @@
+﻿#region
+
+using System.Drawing;
+
+#endregion
+
+namespace Tabster.Core.Printing
+{
+    public abstract class TablaturePrintDocumentSettingsBase
+    {
+        protected TablaturePrintDocumentSettingsBase()
+        {
+            PrintColor = Color.Black;
+        }
+
+        public Color PrintColor { get; set; }
+        public bool DisplayPageNumbers { get; set; }
+        public bool DisplayTimetstamp { get; set; }
+    }
+}
