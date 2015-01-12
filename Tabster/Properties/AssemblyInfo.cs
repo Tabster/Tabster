bba15f5dd8aa84a5fc1db14985fb726ab0dfe6b7
Nate Shoffner <nate.shoffner@gmail.com>
2015-01-12 12:52:59 -0500

    Update copyright.

--- Tabster/Properties/AssemblyInfo.cs
+++ Tabster/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nate Shoffner")]
 [assembly: AssemblyProduct("Tabster")]
-[assembly: AssemblyCopyright("Copyright © 2010-2014")]
+[assembly: AssemblyCopyright("Copyright © 2010-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
