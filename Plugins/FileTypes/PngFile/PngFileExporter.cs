e57e8ee957d7d42716a83d80f89a2f47067b08f0
Nate Shoffner <nate.shoffner@gmail.com>
2016-02-17 01:16:40 -0500

    Use specified font for PNG exporting

--- Plugins/FileTypes/PngFile/PngFileExporter.cs
+++ Plugins/FileTypes/PngFile/PngFileExporter.cs
@@ -4,7 +4,6 @@ using System;
 using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Drawing.Text;
-using System.Windows.Forms;
 using Tabster.Data;
 using Tabster.Data.Processing;
 
@@ -28,45 +27,36 @@ namespace PngFile
             get { return new Version("1.0"); }
         }
 
-        public void Export(ITablatureFile file, string fileName)
+        public void Export(ITablatureFile file, string fileName, TablatureFileExportArguments args)
         {
-            using (var fontDialog = new FontSizeDialog())
+            SizeF size;
+
+            //measure size of text
+            using (var bmp = new Bitmap(1, 1))
             {
-                if (fontDialog.ShowDialog() == DialogResult.OK)
+                using (var g = Graphics.FromImage(bmp))
                 {
-                    //todo use custom font
-                    var font = new Font("Courier New", fontDialog.FontSize);
-
-                    SizeF size;
-
-                    //measure size of text
-                    using (var bmp = new Bitmap(1, 1))
-                    {
-                        using (var g = Graphics.FromImage(bmp))
-                        {
-                            size = g.MeasureString(file.Contents, font);
-                        }
-                    }
-
-                    const int verticalPadding = 10;
-                    const int horizontalPadding = 10;
-
-                    size.Width += (horizontalPadding*2);
-                    size.Height += (verticalPadding*2);
-
-                    using (var bmp = new Bitmap((int) size.Width, (int) size.Height))
-                    {
-                        using (var g = Graphics.FromImage(bmp))
-                        {
-                            g.Clear(Color.White);
-                            g.SmoothingMode = SmoothingMode.AntiAlias;
-                            g.TextRenderingHint = TextRenderingHint.AntiAlias;
-                            g.DrawString(file.Contents, font, Brushes.Black, verticalPadding, horizontalPadding);
-                        }
-
-                        bmp.Save(fileName);
-                    }
+                    size = g.MeasureString(file.Contents, args.Font);
                 }
+            }
+
+            const int verticalPadding = 10;
+            const int horizontalPadding = 10;
+
+            size.Width += (horizontalPadding*2);
+            size.Height += (verticalPadding*2);
+
+            using (var bmp = new Bitmap((int) size.Width, (int) size.Height))
+            {
+                using (var g = Graphics.FromImage(bmp))
+                {
+                    g.Clear(Color.White);
+                    g.SmoothingMode = SmoothingMode.AntiAlias;
+                    g.TextRenderingHint = TextRenderingHint.AntiAlias;
+                    g.DrawString(file.Contents, args.Font, Brushes.Black, verticalPadding, horizontalPadding);
+                }
+
+                bmp.Save(fileName);
             }
         }
 
