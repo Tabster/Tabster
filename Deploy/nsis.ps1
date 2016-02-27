314098a5afba75363f60bc556a68a992471e1b4c
Nate Shoffner <nate.shoffner@gmail.com>
2016-02-27 06:12:42 -0500

    Use PowerShell script for makensis

---
+++ Deploy/nsis.ps1
@@ -0,0 +1,2 @@
+$script = $env:APPVEYOR_BUILD_FOLDER + "\Build\Tabster.nsi"
+& "C:\Program Files (x86)\NSIS\makensis.exe" /DAPPLICATION_VERSION="$env:APPVEYOR_BUILD_VERSION" /DSOLUTION_DIRECTORY="$env:APPVEYOR_BUILD_FOLDER" "$script"'
