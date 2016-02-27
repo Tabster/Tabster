d8e4a4d73c315bbcc8c28ca55be2b8efd4982187
Nate Shoffner <nate.shoffner@gmail.com>
2016-02-27 09:15:26 -0500

    Explicitly define build folder

--- Deploy/nsis.ps1
+++ Deploy/nsis.ps1
@@ -1,4 +1,4 @@
 $script = $env:APPVEYOR_BUILD_FOLDER + "\Build\Tabster.nsi"
 & "C:\Program Files (x86)\NSIS\makensis.exe" /DAPPLICATION_VERSION="$env:APPVEYOR_BUILD_VERSION" /DSOLUTION_DIRECTORY="$env:APPVEYOR_BUILD_FOLDER" "$script"
 $installer = $env:APPVEYOR_BUILD_FOLDER + "\Build\*.exe"
-Move-Item "$installer" "..\"
+Move-Item "$installer" "$env:APPVEYOR_BUILD_FOLDER"
