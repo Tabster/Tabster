846a952c6d3a72e5fbca52c071331bd82665810d
Nate Shoffner <nate.shoffner@gmail.com>
2016-02-27 08:16:14 -0500

    Fix relative path

--- Deploy/nsis.ps1
+++ Deploy/nsis.ps1
@@ -1,3 +1,3 @@
 $script = $env:APPVEYOR_BUILD_FOLDER + "\Build\Tabster.nsi"
 & "C:\Program Files (x86)\NSIS\makensis.exe" /DAPPLICATION_VERSION="$env:APPVEYOR_BUILD_VERSION" /DSOLUTION_DIRECTORY="$env:APPVEYOR_BUILD_FOLDER" "$script"
-Move-Item .\*.exe ..\
+Move-Item ..\Build\*.exe ..\
