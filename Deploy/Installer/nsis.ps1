3dde4a1892cc931fdb2795c1a198aa6b65489510
Nate Shoffner <nate.shoffner@gmail.com>
2016-02-28 08:29:53 -0500

    Testing artifact path conflict

--- Deploy/Installer/nsis.ps1
+++ Deploy/Installer/nsis.ps1
@@ -1,5 +1,5 @@
 & "C:\Program Files (x86)\NSIS\makensis.exe" /DAPPLICATION_VERSION="$env:APPVEYOR_BUILD_VERSION" /DSOLUTION_DIRECTORY="$env:APPVEYOR_BUILD_FOLDER" "$env:APPVEYOR_BUILD_FOLDER\Deploy\Installer\Tabster.nsi"
 
 # move executable to project directory for clean AppVeyor artifact name
-$installer = "$env:APPVEYOR_BUILD_FOLDER\Deploy\Installer\Tabster $env:APPVEYOR_BUILD_VERSION Setup.exe"
+$installer = "$env:APPVEYOR_BUILD_FOLDER\Deploy\Installer\Tabster_$env:APPVEYOR_BUILD_VERSION_Setup.exe"
 Move-Item "$installer" "$env:APPVEYOR_BUILD_FOLDER"
