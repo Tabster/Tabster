464a89e80081ae79a3e4b4c97d23c0e688dc64f2
Nate Shoffner <nate.shoffner@gmail.com>
2016-02-28 04:43:32 -0500

    Restructured deployment directory

---
+++ Deploy/Installer/nsis.ps1
@@ -0,0 +1,5 @@
+& "C:\Program Files (x86)\NSIS\makensis.exe" /DAPPLICATION_VERSION="$env:APPVEYOR_BUILD_VERSION" /DSOLUTION_DIRECTORY="$env:APPVEYOR_BUILD_FOLDER" "$env:APPVEYOR_BUILD_FOLDER\Build\Tabster.nsi"
+
+# move executable to project directory for clean AppVeyor artifact name
+$installer = $env:APPVEYOR_BUILD_FOLDER + "\Deploy\NSIS\Tabster $env:APPVEYOR_BUILD_VERSION Setup.exe"
+Move-Item "$installer" "$env:APPVEYOR_BUILD_FOLDER"
--- Deploy/Portable/portable.ps1
+++ Deploy/Portable/portable.ps1
@@ -1,7 +1,7 @@
-$temp_directory = "$env:APPVEYOR_BUILD_FOLDER\Build\~TEMP"
+$temp_directory = "$env:APPVEYOR_BUILD_FOLDER\Deploy\Portable\~TEMP"
 New-Item "$temp_directory" -type directory
 
-$zip_archive = "$env:APPVEYOR_BUILD_FOLDER\Build\Tabster $env:APPVEYOR_BUILD_VERSION Portable.exe"
+$zip_archive = "$env:APPVEYOR_BUILD_FOLDER\Deploy\Portable\Tabster $env:APPVEYOR_BUILD_VERSION Portable.exe"
 
 & msbuild.exe "$env:APPVEYOR_BUILD_FOLDER\Tabster.sln" /p:Configuration=Portable
 
@@ -14,3 +14,6 @@ Copy-Item "$output_directory\*.config" "$temp_directory"
 
 # copy subfolders recursively
 Get-ChildItem -Path "$output_directory" -Recurse | ?{ $_.PSIsContainer } | copy-item -Destination "$temp_directory" -Force -Container
+
+# move executable to project directory for clean AppVeyor artifact name
+Move-Item "$zip_archive" "$env:APPVEYOR_BUILD_FOLDER"
