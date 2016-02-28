1d83165737054879d1f9310fb3e1d7166b7ceb4c
Nate Shoffner <nate.shoffner@gmail.com>
2016-02-28 07:23:59 -0500

    Fixed 7zip SFX

--- Deploy/Portable/portable.ps1
+++ Deploy/Portable/portable.ps1
@@ -8,16 +8,13 @@ $zip_archive = "$env:APPVEYOR_BUILD_FOLDER\Deploy\Portable\Tabster $env:APPVEYOR
 
 $output_directory = "$env:APPVEYOR_BUILD_FOLDER\Tabster\bin\Portable"
 
-# copy .exe, .dll, .config files
-Copy-Item "$output_directory\*.exe" "$temp_directory"
-Copy-Item "$output_directory\*.dll" "$temp_directory"
-Copy-Item "$output_directory\*.config" "$temp_directory"
-
-# copy subfolders recursively
-Get-ChildItem -Path "$output_directory" -Recurse | ?{ $_.PSIsContainer } | Copy-Item -Destination "$temp_directory" -Force -Container
+# copy files
+Get-ChildItem -Path "$output_directory" | % { 
+  Copy-Item $_.fullname "$temp_directory" -Recurse -Force -Exclude @("*.xml", "*.pdb", "*.manifest", "*.application", "*.vshost.*") 
+}
 
 # zip contents in self-extracting archive
-& "C:\ProgramData\chocolatey\lib\7zip.commandline\7za.exe" a -r -mmt -mx5 -sfx7z.sfx "$zip_archive" "$temp_directory\*.*"
+& 7z "a" "$zip_archive" "-mmt" "-mx5" "-sfx7z.sfx" "-r" "$temp_directory\*.*"
 
 # move executable to project directory for clean AppVeyor artifact name
 Move-Item "$zip_archive" "$env:APPVEYOR_BUILD_FOLDER"
