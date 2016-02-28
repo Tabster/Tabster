d5e9f4cb4b668040ad968a6a60d61f85409bc3e2
Nate Shoffner <nate.shoffner@gmail.com>
2016-02-28 05:56:53 -0500

    Documentation

--- Deploy/Portable/portable.ps1
+++ Deploy/Portable/portable.ps1
@@ -3,19 +3,21 @@ New-Item "$temp_directory" -type directory
 
 $zip_archive = "$env:APPVEYOR_BUILD_FOLDER\Deploy\Portable\Tabster $env:APPVEYOR_BUILD_VERSION Portable.exe"
 
+# build using portable configuration
 & msbuild.exe "$env:APPVEYOR_BUILD_FOLDER\Tabster.sln" /p:Configuration=Portable
 
 $output_directory = "$env:APPVEYOR_BUILD_FOLDER\Tabster\bin\Portable"
 
+# copy .exe, .dll, .config files
 Copy-Item "$output_directory\*.exe" "$temp_directory"
 Copy-Item "$output_directory\*.dll" "$temp_directory"
 Copy-Item "$output_directory\*.config" "$temp_directory"
-Copy-Item "$output_directory\*.config" "$temp_directory"
 
 # copy subfolders recursively
-Get-ChildItem -Path "$output_directory" -Recurse | ?{ $_.PSIsContainer } | copy-item -Destination "$temp_directory" -Force -Container
+Get-ChildItem -Path "$output_directory" -Recurse | ?{ $_.PSIsContainer } | Copy-Item -Destination "$temp_directory" -Force -Container
 
-& 7z a -r "$zip_archive" -mmt -mx5 -sfx7z.sfx "$temp_directory\*.*"
+# zip contents in self-extracting archive
+& 7z a -r -mmt -mx5 -sfx7z.sfx "$zip_archive" "$temp_directory\*.*"
 
 # move executable to project directory for clean AppVeyor artifact name
 Move-Item "$zip_archive" "$env:APPVEYOR_BUILD_FOLDER"
