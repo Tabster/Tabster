8636f9b391902bd476b2b8a80e4360412b614a19
Nate Shoffner <nate.shoffner@gmail.com>
2016-03-31 15:54:15 -0400

    Removed quotes

--- Deploy/prepare_native_plugins.ps1
+++ Deploy/prepare_native_plugins.ps1
@@ -4,7 +4,6 @@ $plugins_dir = "Plugins"
 
 New-Item "$plugins_dir" -type directory
 
-[Net.ServicePointManager]::SecurityProtocol = 'Tls12'
 $wc = New-Object System.Net.WebClient
 
 $j = $wc.DownloadString("http://tabster.org/featured_plugins.json") | ConvertFrom-Json | Select -ExpandProperty featured_plugins
@@ -15,7 +14,8 @@ Foreach ($plugin in $j)
         Write-Host "Downloading $($plugin.download)`n"
         Write-Host [System.IO.Path]::GetFileName($plugin.download)
         $zip_path = [System.IO.Path]::Combine($plugins_dir, [System.IO.Path]::GetFileName($plugin.download))
-        $wc.DownloadFile($plugin.download, "$zip_path")
+        Write-Host "Download Location: " + $zip_path
+        $wc.DownloadFile($plugin.download, $zip_path)
 
         Write-Host "Unzipping $($plugin.download)`n"
         $output_dir = [System.IO.Path]::Combine($plugins_dir, $plugin.name)
