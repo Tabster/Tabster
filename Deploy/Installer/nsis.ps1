b09a866e008a5b70f2948b5ee20b3abdf2ebfe54
Nate Shoffner <nate.shoffner@gmail.com>
2016-04-05 13:12:17 -0400

    Move signing to nsis.ps1

--- Deploy/Installer/nsis.ps1
+++ Deploy/Installer/nsis.ps1
@@ -1,5 +1,9 @@
 & "C:\Program Files (x86)\NSIS\makensis.exe" /DAPPLICATION_VERSION="$env:APPVEYOR_BUILD_VERSION" /DSOLUTION_DIRECTORY="$env:APPVEYOR_BUILD_FOLDER" "$env:APPVEYOR_BUILD_FOLDER\Deploy\Installer\Tabster.nsi"
 
-# move executable to project directory for clean AppVeyor artifact name
 $installer = "$env:APPVEYOR_BUILD_FOLDER\Deploy\Installer\Tabster $env:APPVEYOR_BUILD_VERSION Setup.exe"
+
+# sign the installer
+& "$env:sign_tool sign" /f "$env:APPVEYOR_BUILD_FOLDER\Deploy\tabster.pfx" /p $env:cert_pass /t "http://timestamp.comodoca.com/authenticode" "$installer"
+
+# move executable to project directory for clean AppVeyor artifact name
 Move-Item "$installer" "$env:APPVEYOR_BUILD_FOLDER"
